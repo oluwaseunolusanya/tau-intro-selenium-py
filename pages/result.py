771d4a7f3f682848ec376930644d31387233f2f9
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-07-08 07:23:10 +0100

    Added search input and result page locators.

--- pages/result.py
+++ pages/result.py
@@ -3,7 +3,12 @@ This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
 
+from selenium.webdriver.common.by import By
+
 class DuckDuckGoResultPage:
+
+    RESULT_LINK = (By.CSS_SELECTOR, 'a.result__a')        # Adding result page locator
+    SEARCH_INPUT = (By.ID, 'search_form_input')           # Adding search input locator.
 
     def __init__(self, browser):
         self.browser = browser
