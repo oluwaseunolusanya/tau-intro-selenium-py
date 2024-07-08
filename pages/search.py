ed915911b05312f53e71015ae5b60b97079a804e
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-07-08 07:12:56 +0100

    Added search input locator by element ID.

--- pages/search.py
+++ pages/search.py
@@ -3,7 +3,11 @@ This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
 
+from selenium.webdriver.common.by import By
+
 class DuckDuckGoSearchPage:
+
+    SEARCH_INPUT = (By.ID, 'searchbox_input')    # Adding search input locator.
 
     def __init__(self, browser):
         self.browser = browser
