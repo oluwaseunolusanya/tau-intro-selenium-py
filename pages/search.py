85d3a6390351d8166e1626531fe0b24e5a7ce1b9
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-07-08 07:37:37 +0100

    Imported keys from selenium webdriver.

--- pages/search.py
+++ pages/search.py
@@ -4,8 +4,12 @@ the page object for the DuckDuckGo search page.
 """
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class DuckDuckGoSearchPage:
+
+    # URL
+    URL = 'https://www.duckduckgo.com'
 
     SEARCH_INPUT = (By.ID, 'searchbox_input')    # Adding search input locator.
 
